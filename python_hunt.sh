fa9ae14140be24b1f532c46641e9e9998e5599eb
dave bl <db@d1b.org>
2010-12-09 18:36:56 +1100

    add parser, compiler, zipfile, rexec and bastion to the script
    Signed-off-by: dave bl <db@d1b.org>

--- python_hunt.sh
+++ python_hunt.sh
@@ -1,3 +1,9 @@
-#! /bin/bash
-egrep --color -e "pickle" -e "cPickle" -e "os.system" -e "shell=False" -e "shell" -e "subprocess" -e "popen" -e "input" -e "os.popen" -e  "Cookie" -e "SmartCookie" -e "SerialCookie" -e "pipes" -e "shelve" -e "ast" -e "multiprocessing" -e "tarfile" * -R
+#!/bin/bash
+egrep --color -e "pickle" -e "cPickle" -e "os.system" -e "shell=False" -e "shell" -e "subprocess" -e "popen" -e "input" -e "os.popen" -e  "Cookie" -e "SmartCookie" -e "SerialCookie" -e "pipes" -e "shelve" -e " ast" -e "multiprocessing" -e "tarfile" \
+	-e " parser" \
+	-e " compiler" \
+	-e "zipfile" \
+	-e "rexec" \
+	-e " bastion" \
+	* -R
 
