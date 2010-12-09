16667109340ee072a83ec4c931e1a52642a6b5f5
dave bl <db@d1b.org>
2010-12-09 18:40:37 +1100

    put each module / function (-e parm) onto its own line
    Signed-off-by: dave bl <db@d1b.org>

--- python_hunt.sh
+++ python_hunt.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-egrep --color -e "pickle" -e "cPickle" -e "os.system" -e "shell=False" -e "shell" -e "subprocess" -e "popen" -e "input" -e "os.popen" -e  "Cookie" -e "SmartCookie" -e "SerialCookie" -e "pipes" -e "shelve" -e " ast" -e "multiprocessing" -e "tarfile" \
+egrep --color \
+	-e "pickle" \
+	-e "cPickle" \
+	-e "os.system" \
+	-e "shell=False" \
+	-e "shell" \
+	-e "subprocess" \
+	-e "popen" \
+	-e "input" \
+	-e "os.popen" \
+	-e  "Cookie" \
+	-e "SmartCookie" \
+	-e "SerialCookie" \
+	-e "pipes" \
+	-e "shelve" \
+	-e " ast" \
+	-e "multiprocessing" \
+	-e "tarfile" \
 	-e " parser" \
 	-e " compiler" \
 	-e "zipfile" \
