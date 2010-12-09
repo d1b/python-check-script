f182c2bc84d62220c245e81ea4bd91181910c899
dave bl <db@d1b.org>
2010-12-09 18:24:19 +1100

    add multiprocessing and tarfile
    Signed-off-by: dave bl <db@d1b.org>

--- python_hunt.sh
+++ python_hunt.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-egrep --color -e "pickle" -e "cPickle" -e "os.system" -e "shell=False" -e "shell" -e "subprocess" -e "popen" -e "input" -e "os.popen" -e  "Cookie" -e "SmartCookie" -e "SerialCookie" -e "pipes" -e "shelve" -e "ast" * -R
+egrep --color -e "pickle" -e "cPickle" -e "os.system" -e "shell=False" -e "shell" -e "subprocess" -e "popen" -e "input" -e "os.popen" -e  "Cookie" -e "SmartCookie" -e "SerialCookie" -e "pipes" -e "shelve" -e "ast" -e "multiprocessing" -e "tarfile" * -R
 
